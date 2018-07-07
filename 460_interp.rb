a2382c8b9232d5d9b08d6fb4cbb33a0b9414ef24
Goh Matsumoto <urimaro@gmail.com>
2018-07-07 16:59:12 +0900

    P57 4.6 四則演算に対応

---
+++ 460_interp.rb
@@ -0,0 +1,36 @@
+require 'minruby'
+
+def evaluate(tree)
+  case tree[0]
+  when "lit"
+    tree[1]
+  when "+"
+    left  = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left + right
+  when "-"
+    left  = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left - right
+  when "*"
+    left  = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left * right
+  else
+    left  = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left / right
+  end
+end
+
+# 1. 計算式の文字列を読み込む
+str = gets
+
+# 2. 計算式の文字列を計算の木に変換する
+tree = minruby_parse(str)
+
+# 3. 計算の木を実行(計算)する
+answer = evaluate(tree)
+
+# 4. 計算結果を出力する
+p(answer)
