7d9b97c126e4cd0b943162a104f060d9f3022b11
Goh Matsumoto <urimaro@gmail.com>
2018-07-08 17:04:18 +0900

    P71 5.3.3 環境：変数名と値の対応関係

---
+++ 530_interp.rb
@@ -0,0 +1,36 @@
+require 'minruby'
+
+def evaluate(tree, env)
+  case tree[0]
+  when "lit"
+    tree[1]
+  when "+"
+    evaluate(tree[1], env) + evaluate(tree[2], env)
+  when "-"
+    evaluate(tree[1], env) - evaluate(tree[2], env)
+  when "*"
+    evaluate(tree[1], env) * evaluate(tree[2], env)
+  when "/"
+    evaluate(tree[1], env) / evaluate(tree[2], env)
+  when "func_call" # 仮の実装
+    p(evaluate(tree[2], env))
+  when "stmts"
+    i = 1
+    last = nil
+    while tree[i] != nil
+      last = evaluate(tree[i], env)
+      i = i + 1
+    end
+    last
+  end
+end
+
+# 1. 計算式の文字列を読み込む
+str = minruby_load()
+
+# 2. 計算式の文字列を計算の木に変換する
+tree = minruby_parse(str)
+
+# 3. 計算の木を実行(計算)する
+env = {}
+evaluate(tree, env)
