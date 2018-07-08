e9d3ade9352928eca053ae5bb5f3b3befd854441
Goh Matsumoto <urimaro@gmail.com>
2018-07-08 14:07:31 +0900

    P66 5.2.3 複文の実装

---
+++ 520_interp.rb
@@ -0,0 +1,35 @@
+require 'minruby'
+
+def evaluate(tree)
+  case tree[0]
+  when "lit"
+    tree[1]
+  when "+"
+    evaluate(tree[1]) + evaluate(tree[2])
+  when "-"
+    evaluate(tree[1]) - evaluate(tree[2])
+  when "*"
+    evaluate(tree[1]) * evaluate(tree[2])
+  when "/"
+    evaluate(tree[1]) / evaluate(tree[2])
+  when "func_call" # 仮の実装
+    p(evaluate(tree[2]))
+  when "stmts"
+    i = 1
+    last = nil
+    while tree[i] != nil
+      last = evaluate(tree[i])
+      i = i + 1
+    end
+    last
+  end
+end
+
+# 1. 計算式の文字列を読み込む
+str = minruby_load()
+
+# 2. 計算式の文字列を計算の木に変換する
+tree = minruby_parse(str)
+
+# 3. 計算の木を実行(計算)する
+answer = evaluate(tree)
